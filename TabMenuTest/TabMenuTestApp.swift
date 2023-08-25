3c80779e2680bb842b36124bcf15cbae3bfde42b
Chevireddy, Sraavan <sraavan.chevireddy@truflapp.com>
2023-08-25 22:00:42 +0530

    Initial Commit

---
+++ TabMenuTest/TabMenuTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabMenuTestApp.swift
+//  TabMenuTest
+//
+//  Created by Sraavan Chevireddy on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TabMenuTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
